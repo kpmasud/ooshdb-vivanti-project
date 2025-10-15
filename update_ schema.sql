7abb56ad2352bc833146e236f95820d44d59090a
imasudrana <kpmasud@gmail.com>
2025-10-15 23:32:13 +1100

    updated the schema

--- update_ schema.sql
+++ update_ schema.sql
@@ -41,43 +41,69 @@ ON DELETE               SET NULL;
 
 
 -- Check delivery_methods table structure
-SELECT column_name, data_type 
-FROM information_schema.columns 
-WHERE table_name = 'delivery_methods';
+SELECT 
+column_name,            data_type 
+FROM                    information_schema.columns 
+WHERE 
+table_name              = 'delivery_methods';
 
 -- Check orders table structure
-SELECT column_name, data_type 
-FROM information_schema.columns 
-WHERE table_name = 'orders';
+SELECT 
+column_name,            data_type 
+FROM                    information_schema.columns 
+WHERE 
+table_name              = 'orders';
 
-ALTER TABLE orders
-ADD CONSTRAINT orders_delivery_method_id_fkey 
-FOREIGN KEY (delivery_method_id)
-REFERENCES delivery_methods (delivery_id) 
-ON UPDATE CASCADE
-ON DELETE SET NULL;
+-- we can see that delivery_methods table has a primary key column called delivery_id
+-- so we need to update the foreign key constraint on orders table
+-- referencing delivery_methods table
+-- referencing delivery_method_id column in orders table
+-- so now we can see the correct foreign key constraint command
+
+ALTER TABLE             orders
+ADD CONSTRAINT          orders_delivery_method_id_fkey 
+FOREIGN KEY             (delivery_method_id)
+REFERENCES              delivery_methods (delivery_id) 
+ON UPDATE               CASCADE
+ON DELETE               SET NULL;
 
 -- Check the foreign key constraint
+-- we can check the foreign key constraint using information_schema
+-- we can use the following query to check the foreign key constraint
 
-SELECT tc.constraint_name, tc.table_name, kcu.column_name, 
-       ccu.table_name AS foreign_table_name,
-       ccu.column_name AS foreign_column_name 
-FROM information_schema.table_constraints AS tc 
-JOIN information_schema.key_column_usage AS kcu
-  ON tc.constraint_name = kcu.constraint_name
-JOIN information_schema.constraint_column_usage AS ccu
-  ON ccu.constraint_name = tc.constraint_name
-WHERE tc.constraint_type = 'FOREIGN KEY' 
-  AND tc.table_name = 'orders';
+
+SELECT                  tc.constraint_name, 
+                        tc.table_name, 
+                        kcu.column_name, 
+                        ccu.table_name 
+AS                      foreign_table_name,
+                        ccu.column_name  
+AS                      foreign_column_name 
+FROM                    information_schema.table_constraints 
+AS                      tc 
+JOIN                    information_schema.key_column_usage 
+AS                      kcu
+ON                      
+                        tc.constraint_name = kcu.constraint_name
+
+JOIN                    information_schema.constraint_column_usage 
+AS                      ccu
+ON                     
+                        ccu.constraint_name = tc.constraint_name
+
+WHERE                   
+                        tc.constraint_type = 'FOREIGN KEY' 
+
+AND                    
+                         tc.table_name = 'orders';
 
 -- Now we have added foreign key constraint on orders table referencing delivery_methods table
 -- referencing delivery_method_id column in orders table
 -- so now we can see the foreign key constraint is added successfully   
 
 
---  
-
-
 -- verify the changes
 
 SELECT * FROM orders;
+
+
